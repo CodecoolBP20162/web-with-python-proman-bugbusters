72757a3c182873691efb15fc20bc957e9e5c2dbe
Mátyás Krista <nincs.matyas@gmail.com>
2017-03-13 23:33:33 +0100

    Testing modals for ProMan

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app.secret_key = 'development key'
 
 @app.route("/")
 def home():
-    return render_template("")
+    return render_template("index_test.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
