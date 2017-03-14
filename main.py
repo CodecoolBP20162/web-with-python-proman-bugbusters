05ca492ad95cd6c316852bb26e9dd9051e0bafbb
Anna Racz <nnaracz@gmail.com>
2017-03-14 10:51:35 +0100

    basic design created

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ app.secret_key = 'development key'
 
 @app.route("/")
 def home():
-    return render_template("")
+    return render_template("home.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
