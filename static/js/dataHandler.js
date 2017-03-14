187507fde9c8c94975de4adbdfb6146972c14d21
davidkulchar <davidkulchar@gmail.com>
2017-03-14 22:14:29 +0100

    retrieving boards works with proper css forming

--- static/js/dataHandler.js
+++ static/js/dataHandler.js
@@ -4,9 +4,15 @@
 
 // };
 
+var checkStorage = function () {
+    if (typeof (Storage) !== "undefined") {
+        return True;
+    } else {
+        return False;
+    }
+};
+
 var generateData = function () {
-    // Check browser support
-    if (checkStorage) {
         // Create dictionary
         var boards = {};
         boards.board1 = { title: "Board #1", description: "First board", timestamp: Date.now(), cards: [] };
@@ -23,7 +29,6 @@ var generateData = function () {
         // Store
         // Convert to JSON file and save to storage
         localStorage.boards = JSON.stringify(boards);
-    };
 };
 
 var retrieveData = function (data) {
@@ -32,7 +37,7 @@ var retrieveData = function (data) {
         return JSON.parse(localStorage.getItem(data));
     } else {
         return {}
-    };
+    }
 
 };
 
@@ -41,30 +46,44 @@ var checkStorage = function () {
         return True;
     } else {
         return False;
-    };
+    }
+};
 
+var retrieveData = function (data) {
+    return JSON.parse(localStorage.getItem(data));
 };
 
 var getBoards = function(data) {
     var allBoards = retrieveData(data);
-    var boards = []
     for (var i in allBoards) {
-      var div = document.createElement("div");
-      div.className = "col-lg-3 col-md-4 col-sm-6 col-xs-12";
-      div.className = " 'project project-grey draggable' draggable='true' data-toggle='modal' data-target='.board-modal'";
-      var h3 = document.createElement("h3");
-      var text = document.createTextNode(allBoards[i].title);
-      h3.appendChild(text);
-      div.appendChild(h3);
-
+        var projectContent = createProjectContent(allBoards[i]);
+        var ediv = decorateContext("project project-grey draggable draggable='true' data-toggle='modal' data-target='.board-modal'", projectContent);
+        var div = decorateContext("col-lg-3 col-md-4 col-sm-6 col-xs-12", ediv);
         document.getElementById("result").appendChild(div);
-
-  };
-
+  }
 };
 
+var createProjectContent = function (board) {
+    var projectContent = document.createElement("div");
+    projectContent.className = "project-content";
+    var h3 = document.createElement("h3");
+    var text = document.createTextNode(board.title);
+    h3.className = "lead";
+    h3.appendChild(text);
+    var p = document.createElement("p");
+    var description = document.createTextNode(board.description);
+    p.appendChild(description);
+    projectContent.appendChild(h3);
+    projectContent.appendChild(p);
+    return  projectContent;
+};
 
-
+var decorateContext = function(name, context) {
+    var div = document.createElement("div");
+    div.className = name;
+    div.appendChild(context);
+    return div
+};
 
 generateData();
 var boards = retrieveData("boards");
