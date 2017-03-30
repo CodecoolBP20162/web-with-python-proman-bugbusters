e8e6ebaf681ca4b361ce8cbc1cd55036f6725ef8
Mátyás Krista <nincs.matyas@gmail.com>
2017-03-30 21:00:40 +0200

    m

--- connect_database.py
+++ connect_database.py
@@ -11,6 +11,6 @@ class ConnectDatabase:
             print("You need to create a database and store its name in a file named 'parameter.txt'. \
                   For more info, head over to the README")
 
-    db = PostgresqlDatabase('d4ekbpq0tu6bop', host='ec2-176-34-113-15.eu-west-1.compute.amazonaws.com',
-                            user='kkblxxpiveukqs', port=5432,
-                            password='7d0cbbd8417c09332a99e8f92a605c9dd2bf7e782a63e3a837bf0a46ee3f5bf3',)
+    db = PostgresqlDatabase(database='d4ekbpq0tu6bop', host='ec2-176-34-113-15.eu-west-1.compute.amazonaws.com',
+                            user='kkblxxpiveukqs', port='5432',
+                            password='7d0cbbd8417c09332a99e8f92a605c9dd2bf7e782a63e3a837bf0a46ee3f5bf3')
